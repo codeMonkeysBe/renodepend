58bf8c4b2367ae3dd46d7ce04c223c875f7e367b
Niki Van Cleemput <niki.vancleemput@auth0.com>
2022-07-13 11:15:48 +0200

    improve output

--- get-info.sh
+++ get-info.sh
@@ -1,1 +1,2 @@
-curl -H "Accept: application/vnd.github+json" -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/repos/codeMonkeysBe/renodepend/pulls | jq '.[] | select(.head.ref|startswith("renovate")) | .labels[].name'
+curl -H "Accept: application/vnd.github+json" -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/repos/codeMonkeysBe/renodepend/pulls | jq \
+	'.[] | select(.head.ref|startswith("renovate")) | { meta: [.labels[].name|split(":") | { (.[0]) : .[1] }] | add, title:.title, created:.created_at}'
